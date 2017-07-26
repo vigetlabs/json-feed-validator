705e23346d965200a3048792ddc1032097c12d10
David Eisinger <david.eisinger@viget.com>
2017-07-25 22:53:39 -0400

    Better error display; UI improvements

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ require "json-schema"
 
 schema = File.read("schema.json")
 
+helpers do
+  def format_error_message(msg)
+    CGI.escapeHTML(msg)
+      .squeeze("\n")
+      .gsub(/^[ ]+/) { |s| "&nbsp;" * s.length }
+      .gsub(Regexp.new('in schema [-#\w]+$'), "")
+      .gsub(/\n/, "<br>")
+  end
+end
+
 get "/" do
   @data = ""
   @errors = []
@@ -14,10 +24,8 @@ post "/validate" do
   @data = params[:data]
 
   begin
-    JSON.parse(@data)
-
-    @errors = JSON::Validator.fully_validate(schema, @data)
     @data = JSON.pretty_generate(JSON.parse(@data))
+    @errors = JSON::Validator.fully_validate(schema, @data)
   rescue => ex
     @errors = [ex.message]
   end
